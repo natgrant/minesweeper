c282e3491860d86ecf22a144b8ea7e4f34d6e3f5
Nat Grant <email.natgrant@gmail.com>
2018-12-17 10:45:12 +1300

    Defining board object

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+
+var board = {
+  cells: [
+    {row: 0, col: 0},
+    {row: 0, col: 1},
+    {row: 0, col: 2},
+    {row: 1, col: 2},
+    {row: 1, col: 1},
+    {row: 1, col: 0},
+    {row: 2, col: 0},
+    {row: 2, col: 1},
+    {row: 2, col: 2}
+  ]
+};
+
+
+
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
